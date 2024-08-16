1f39e4c9d43c2564af9076ddd14c30fb7a4840ef
poliana <114242284+polinhawrublak@users.noreply.github.com>
2024-08-16 08:58:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,103 @@
+export const perguntas = [
+    {
+        enunciado: "O que é IA?",
+        alternativas: [
+            {
+                texto: "É um ramo da ciência que se concentra no desenvolvimento de sistemas e algoritmos capazes de realizar tarefas que normalmente exigem inteligência humana.",
+                afirmacao: [
+                "Isso mesmo! Continue pesquisando sobre IA." ,
+                "é um conjunto de tecnologias que permitem aos computadores executar uma variedade de funções avançadas, incluindo a capacidade de ver, entender e traduzir idiomas falados e escritos, analisar dados, fazer recomendações e muito mais."
+            ]
+            },
+            {
+                texto: "É o índice de aumento de expectativa de vida no mundo.",
+                afirmacao: [ 
+                    "O índice de aumento de expectativa de vida no mundo não está relacionado com a IA." ,
+                    "É um importante indicador social, porém não é a definição de IA." 
+            ]
+            }
+               
+        ]
+    },
+    {
+        enunciado: "Como a inteligência artificial pode ser útil no dia-a-dia?",
+        alternativas: [
+            {
+                texto: "Com assistentes pessoais, casas inteligentes, na educação, na medicina, nas empresas e na indústria.",
+                afirmacao: [
+                    "A inteligência artificial tem sido cada vez mais utilizada no nosso dia-a-dia, em diversas áreas da nossa vida." ,
+                    "A inteligência artificial contutribui e contribuirá para o aumento da automação de diferentes tarefas."
+                ]
+            },
+            {
+                texto: "Levando café da manhã na sua cama.",
+                afirmacao: [
+                    "A IA não pode fazer isso.",
+                    "A IA não é um robô, portanto não pode fazer isso."
+                ]
+
+            }
+               
+        ]
+    },
+    {
+        enunciado: "Quais são os 3 pilares da IA?",
+        alternativas: [
+            {
+                texto: "Tecnologia, cultura, vendas.",
+                afirmacao: [ 
+                    "Isso mesmo! Esses pilares são muito importantes.", 
+                    "A confiança, a segurança dos dados e a promoção contínua da inovação são pilares essenciais para garantir que a IA seja implementada de maneira ética e eficaz."
+                ]
+            },
+            {
+                texto: "Lutas marciais, balé, culinária",
+                afirmacao: [ 
+                    "Esses não são os pilares da IA.", 
+                    "Essa não é a resposta correta, estude mais!"
+                ]
+            }
+              
+        ]
+    },
+    {
+        enunciado: "Qual o principal objetivo da IA?",
+        alternativas: [
+            {
+                texto: "Extinguir a raça humana.",
+                afirmacao: [ 
+                    "Esse é um pensamento errado que infelizmente muitas pessoas tem.", 
+                    "A IA surgiu para facilitar nossas vidas, e não para extinguir elas. "
+            ]
+            },
+            {
+                texto: "Automatizar tarefas.",
+                afirmacao: [
+                    "Correto! Continue estudando...", 
+                    "A IA estuda, desenvolve e emprega máquinas para realizar atividades humanas de maneira autônoma."
+                ]
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Quais são os principais tipos de IA utilizados na indústria?",
+        alternativas: [
+            {
+                texto: "Aprendizado de máquina, processamento de linguagem natural, visão computacional e robótica inteligente.",
+                afirmacao: [
+                    "A IA na indústria é algo revulucionário!",
+                    "A IA na indústria permite a implementação de sistemas de manutenção preditiva, utilizando sensores e uma rede interconectada para monitorar o desempenho das máquinas em tempo real."
+                ]
+            },
+            {
+                texto: "A IA não é utilizada na indústria.",
+                afirmacao: [
+                    "Ela é muito utilizada na indústria",
+                    "Essa afirmação não está correta, pois a IA é sim utilizada na indústria em diversos setores."
+                ]
+            }
+            
+        ]
+    }
+];
